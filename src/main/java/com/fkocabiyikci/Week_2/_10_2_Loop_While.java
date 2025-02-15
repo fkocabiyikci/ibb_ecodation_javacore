38be59f7a09fd0e820294809836c9080da8996b9
fkoca <fatmaa.kocabiyikcii@gmail.com>
2025-02-15 14:35:12 +0300

    while

--- src/main/java/com/fkocabiyikci/Week_2/_10_2_Loop_While.java
+++ src/main/java/com/fkocabiyikci/Week_2/_10_2_Loop_While.java
@@ -4,9 +4,9 @@ package com.fkocabiyikci.Week_2;
 public class _10_2_Loop_While {
 
     public static void main(String[] args) {
-        // i++
-        // i=i+1
-        // i+=1
+        // i++  i'nin değerini 1 artırır.
+        // i=i+1 i değişkeninin mevcut değerine 1 ekleyerek yeni değerini atar.
+        // i+=1 i'nin değerine 1 ekler ve sonucu i'ye geri atar.
         int i = 1;
         while (i <= 10) {
             System.out.print(i + " - ");
