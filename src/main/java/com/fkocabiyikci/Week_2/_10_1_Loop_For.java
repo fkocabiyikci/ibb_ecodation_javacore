04ee5b8b8c81a89720beb63266e2c878258ea7cd
fkoca <fatmaa.kocabiyikcii@gmail.com>
2025-02-15 14:35:00 +0300

    for

--- src/main/java/com/fkocabiyikci/Week_2/_10_1_Loop_For.java
+++ src/main/java/com/fkocabiyikci/Week_2/_10_1_Loop_For.java
@@ -4,9 +4,9 @@ package com.fkocabiyikci.Week_2;
 public class _10_1_Loop_For {
 
     public static void main(String[] args) {
-        // i++
-        // i=i+1
-        // i+=1
+        // i++  i'nin değerini 1 artırır.
+        // i=i+1 i değişkeninin mevcut değerine 1 ekleyerek yeni değerini atar.
+        // i+=1 i'nin değerine 1 ekler ve sonucu i'ye geri atar.
         for (int i = 1; i <=10 ; i++) {
             System.out.print(i+" - ");
         }
