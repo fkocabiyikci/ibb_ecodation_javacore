592e23468005de3c0e1884b615f82bafcf28b8a5
fkoca <fatmaa.kocabiyikcii@gmail.com>
2025-02-15 17:23:45 +0300

    Formatter.close

---
+++ src/main/java/com/fkocabiyikci/Week_2/_12_2_StringFormat.java
@@ -0,0 +1,23 @@
+package com.fkocabiyikci.Week_2;
+
+
+import java.util.Formatter;
+
+// Formatter
+// s
+// d
+// f
+public class _12_2_StringFormat {
+
+    // formatter1
+    public static void formatter1(){
+        Formatter formatter= new Formatter();
+        formatter.format(" Merhabalar Adınız: %s, T.C: %d, Fiyat: %f ","Hamit",11223344,44.23);
+        System.out.println(formatter);
+        formatter.close(); // Belleği serbest bırakma
+    }
+
+    public static void main(String[] args) {
+        formatter1();
+    }
+}
